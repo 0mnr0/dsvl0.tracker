ffd25a6eeccb8dfa682f22be1801239885b5f2dc
JustDev <savelijln0@gmail.com>
2025-08-30 21:30:03 +0400

    Added tray icon to stop program

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import threading
 import subprocess
 from trackerConfiguration import *
 
+processExited = False
+
 def getAppList():
     # Get tasklist
     #tasklist = subprocess.check_output("tasklist", shell=False)
@@ -25,9 +27,11 @@ def getAppList():
 
 
 def appTracker():
-    while True:
+    while not processExited:
         appList = getAppList()
 
+        if processExited:
+            break
 
         ActiveApps = []
         for app in appList:
@@ -36,17 +40,63 @@ def appTracker():
                     ActiveApps.append(app)
 
 
-        print("ActiveApps:", ActiveApps)
-        fetchResult = requests.post(trackerURL, json={"secretCode": secretCode, "activeApps": ActiveApps})
+
+        if not processExited:
+            fetchResult = requests.post(trackerURL, json={"secretCode": secretCode, "activeApps": ActiveApps})
+        else:
+            break
 
         if fetchResult.status_code != 200:
             print(f"Failed to push data to tracker, refetching in 20 seconds (CODE: {fetchResult.status_code})")
-            time.sleep(20)
+            for i in range(20):
+                time.sleep(1)
+                if processExited:
+                    break
+            if processExited:
+                break
             requests.post(trackerURL, json={"secretCode": secretCode, "activeApps": ActiveApps})
 
-        time.sleep(60)
+        for i in range(60):
+            time.sleep(1)
+            if processExited:
+                break
 
+
+
+def TRAY():
+    import sys
+    import threading
+    from pystray import Icon, MenuItem, Menu
+    from PIL import Image, ImageDraw
+
+    def create_image():
+        # Простая картинка (иконка) 16x16
+        image = Image.new("RGB", (64, 64), "black")
+        draw = ImageDraw.Draw(image)
+        draw.rectangle([16, 16, 48, 48], fill="white")
+        return image
+
+    def on_exit(icon, item):
+        icon.stop()
+        global processExited
+        processExited = True
+        sys.exit(0)
+
+    def trayStart():
+        # Создаём меню
+        menu = Menu(
+            MenuItem("dsvl0.tracker", None, enabled=False),
+            Menu.SEPARATOR,
+            MenuItem("Выход", on_exit)
+        )
+        # Запускаем иконку
+        icon = Icon("test", create_image(), menu=menu)
+        icon.run()
+
+    trayStart()
 
 
 if __name__ == "__main__":
+    t = threading.Thread(target=TRAY, daemon=False)
+    t.start()
     appTracker()
