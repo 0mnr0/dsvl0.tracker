832dbcaffc7eb9e9c6a9173dd8fa8889c3f4a3fd
JustDev <savelijln0@gmail.com>
2025-08-30 23:36:40 +0400

    typo

--- scripts/service.start.cmd
+++ scripts/service.start.cmd
@@ -4,11 +4,11 @@ set SRC=dsvl0.tracker.exe
 
 set STARTUP=%APPDATA%\dsvl0
 if not exist "%STARTUP%\dsvl0.tracker.exe" (
-	msg %username% "Service is not instaled!"
+	msg %username% "Service is not installed!"
     exit
 )
 if not exist "%STARTUP%\run_silent.vbs" (
-	msg %username% "Service is not instaled!"
+	msg %username% "Service is not installed!"
     exit
 )
 call "%STARTUP%\run_silent.vbs"
