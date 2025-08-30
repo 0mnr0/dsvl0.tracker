1f4ff8b33bc69cf0bf3a3a7816aeb61576daad3c
JustDev <savelijln0@gmail.com>
2025-08-30 18:15:32 +0400

    init

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import requests
+import time
+import threading
+import subprocess
+from trackerConfiguration import *
+
+def getAppList():
+    # Get tasklist
+    #tasklist = subprocess.check_output("tasklist", shell=False)
+
+    tasklist = subprocess.run(["chcp", "1251", ">", "nul", "&&", "tasklist"], shell=True, capture_output=True, text=True)
+    tasklist = tasklist.stdout
+    appList = tasklist.split("\n")
+    for i in range(len(appList)):
+        if ".exe" in appList[i]:
+            appList[i] = appList[i].split(".exe")[0] + ".exe"
+        else:
+            appList[i] = ""
+
+    while "" in appList:
+        appList.remove("")
+
+    return appList
+
+
+def appTracker():
+    while True:
+        appList = getAppList()
+
+
+        ActiveApps = []
+        for app in appList:
+            for tracker in trackingData:
+                if app == tracker["process"]:
+                    ActiveApps.append(app)
+
+
+        print("ActiveApps:", ActiveApps)
+        fetchResult = requests.post(trackerURL, json={"secretCode": secretCode, "activeApps": ActiveApps})
+
+        if fetchResult.status_code != 200:
+            print("Failed to push data to tracker, refetching in 20 seconds")
+            time.sleep(20)
+            requests.post(trackerURL, json={"secretCode": secretCode, "activeApps": ActiveApps})
+
+        time.sleep(60)
+
+
+
+if __name__ == "__main__":
+    appTracker()
---
+++ trackerConfiguration.py
@@ -0,0 +1,34 @@
+trackerURL = "https://dsvl0.space/devTracker/push"
+secretCode = None
+try: secretCode = open('secret.key', 'r').read()
+except: raise Exception("Secret key not found (secret.key)")
+
+trackingData = [
+    {
+        "process": "PEAK.exe",
+        "isGame": True,
+        "displayName": "PEAK",
+    }, {
+        "process": "7DaysToDie.exe",
+        "isGame": True,
+        "displayName": "7 Days To Die",
+    }, {
+        "process": "Phasmophobia.exe",
+        "isGame": True,
+        "displayName": "Phasmophobia",
+    }, {
+        "process": "FSD.exe",
+        "isGame": True,
+        "displayName": "Deep Rock Galactic",
+    },
+    {
+        "process": "Minecraft.exe",
+        "isGame": True,
+        "displayName": "Minecraft",
+    },
+    {
+        "process": "UltimateChickenHorse.exe",
+        "isGame": True,
+        "displayName": "Ultimate Chicken Horse",
+    }
+]
